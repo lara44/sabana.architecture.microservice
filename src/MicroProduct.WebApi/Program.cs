080a2031f399da03a4d0a8bdd3404b7a5f34ca5d
lara44 <43456256+lara44@users.noreply.github.com>
2025-10-23 17:56:31 -0500

    [UPDATE] Update root endpoint response message for consistency

--- src/MicroProduct.WebApi/Program.cs
+++ src/MicroProduct.WebApi/Program.cs
@@ -18,7 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "MicroProduct.WebApi is running...");
+app.MapGet("/", () => "WebApi is running...");
 
 app.MapControllers();
 app.Run();
